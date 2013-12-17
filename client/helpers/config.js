4126217f07a9e456138d8c7c2c4066ca70633881
Esteban Contreras <socialnerdia@gmail.com>
2013-12-17 01:32:39 -0800

    Update config.js
    Added facebook and changed twitter via to socialnerdia

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -16,15 +16,15 @@ EpicEditorOptions={
 SharrreOptions={
 	share: {
 	  googlePlus: true,
-	  // facebook: true,
+	  facebook: true,
 	  twitter: true,
 	},
 	buttons: {
 	  googlePlus: {size: 'tall', annotation:'bubble'},
-	  // facebook: {layout: 'box_count'},
+	  facebook: {layout: 'box_count'},
 	  twitter: {
 	    count: 'vertical',
-	    via: 'TelescopeApp'
+	    via: 'Socialnerdia'
 	  },
 	},
 	enableHover: false,
